f047432e685e8f7157e6ebc03cb9addb96163e76
Milind Bhor <Milind.Bhor@sprint.com>
2018-09-22 13:01:47 -0500

    Added Insert,Update and Delete Queries of JDBCTemplate

--- src/main/java/com/milind/jpa/hibernate/jpabasics/UserDaoServiceCOmmandRunner.java
+++ src/main/java/com/milind/jpa/hibernate/jpabasics/UserDaoServiceCOmmandRunner.java
@@ -1,5 +1,6 @@
 package com.milind.jpa.hibernate.jpabasics;
 
+import com.milind.jpa.hibernate.jpabasics.entities.Person;
 import com.milind.jpa.hibernate.jpabasics.services.PersonDAOService;
 
 import org.slf4j.Logger;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 
 
 @Component
@@ -26,6 +28,24 @@ public class UserDaoServiceCOmmandRunner implements CommandLineRunner {
 
         logs.info("\n ==========================================");
         logs.info("\n All users \n :{}",personDAOService.findAll());
+        logs.info("\n ==========================================");
+        logs.info("\n Find By id  \n :{}",personDAOService.findById(10001));
+        //logs.info("\n ==========================================");
+        //logs.info("\n Find By id  \n :{}",personDAOService.findByLocation("Canada"));
+        logs.info("\n ==========================================");
+        logs.info("\n Delete By id  \n :{}",personDAOService.deleteByID(10003));
+        logs.info("\n All users \n :{}",personDAOService.findAll());
+        logs.info("\n ==========================================");
+        logs.info("\n Update record of user Id 10002");
+        logs.info("\n Update By Id \n :{}",personDAOService.updatePersonData(new Person(10002,"Ganesha","US",new Date())));
+        logs.info("\n All users \n :{}",personDAOService.findAll());
+        logs.info("\n ==========================================");
+        logs.info("\n Insert new record 10004");
+        logs.info("\n Update By Id \n :{}",personDAOService.insertPersonData(new Person(10004,"Ganesha","US",new Date())));
+        logs.info("\n All users \n :{}",personDAOService.findAll());
+
+
+
 
     }
 }
--- src/main/java/com/milind/jpa/hibernate/jpabasics/services/PersonDAOService.java
+++ src/main/java/com/milind/jpa/hibernate/jpabasics/services/PersonDAOService.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -17,5 +18,29 @@ public class PersonDAOService {
 
     public List<Person> findAll(){
        return jdbcTemplate.query("select * from person",new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    public Person findById(int id){
+        return jdbcTemplate.queryForObject("select * from person where id=?",new Object[]{id},
+                new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    //Not working code... Find out how to get multiple objects
+    public List<Person> findByLocation(String location){
+        return jdbcTemplate.queryForList("select * from person where location=?",Person.class,location);
+    }
+
+    public int deleteByID(int id){
+        return jdbcTemplate.update("delete from person where id=?",new Object[]{id});
+    }
+
+    public int insertPersonData(Person person){
+        return jdbcTemplate.update("insert into person values(?,?,?,?)",
+                new Object[]{person.getId(),person.getName(),person.getLocation(),new Timestamp(person.getBirthDate().getTime())});
+    }
+
+    public int updatePersonData(Person person){
+        return jdbcTemplate.update("update person set name=?,location=?,birth_Date=? where id=?",
+                new Object[]{person.getName(),person.getLocation(),new Timestamp(person.getBirthDate().getTime()),person.getId()});
     }
 }
